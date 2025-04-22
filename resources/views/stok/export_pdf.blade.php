e1f1395d3f3e5b2ad3ca1ef370dc0422406947ec
pusatalamsemestaa <pusatalamsemestaaa@gmail.com>
2025-04-22 22:24:30 +0700

    tambahan Tugas 3

--- resources/views/stok/export_pdf.blade.php
+++ resources/views/stok/export_pdf.blade.php
@@ -1,165 +1,99 @@
-@extends('layouts.template')
-
-@section('content')
-<div class="card card-outline card-primary">
-    <div class="card-header">
-        <h3 class="card-title">{{ $page->title }}</h3>
-        <div class="card-tools">
-            <button onclick="modalAction('{{ url('/stok/import') }}')" class="btn btn-info ">Import Stok</button>
-            {{-- <a class="btn btn-sm btn-primary mt-1" href="{{ url('stok/create') }}">Tambah</a> --}}
-            <a href="{{ url('/stok/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i> Export Stok</a>
-            <a href="{{ url('/stok/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export Stok</a>  
-            <button onclick="modalAction('{{ url('stok/create_ajax') }}')" class="btn btn-success">Tambah
-            Ajax</button>
-        </div>
-    </div>
-    <div class="card-body">
-        @if (session('success'))
-            <div class="alert alert-success">{{ session('success') }}</div>
-        @endif
-        @if (session('error'))
-            <div class="alert alert-danger">{{ session('error') }}</div>
-        @endif
-
-        {{-- Filter --}}
-        <div class="row">
-            <div class="col-md-4">
-                <div class="form-group">
-                    <label for="barang_id">Barang</label>
-                    <select class="form-control" id="barang_id" name="barang_id">
-                        <option value="">- Semua Barang -</option>
-                        @foreach($barang as $b)
-                            <option value="{{ $b->barang_id }}">{{ $b->barang_nama }}</option>
-                        @endforeach
-                    </select>
-                    <small class="form-text text-muted">Filter berdasarkan Barang</small>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="form-group">
-                    <label for="user_id">User</label>
-                    <select class="form-control" id="user_id" name="user_id">
-                        <option value="">- Semua User -</option>
-                        @foreach($user as $u)
-                            <option value="{{ $u->user_id }}">{{ $u->username }}</option>
-                        @endforeach
-                    </select>
-                    <small class="form-text text-muted">Filter berdasarkan User</small>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="form-group">
-                    <label for="supplier_id">Supplier</label>
-                    <select class="form-control" id="supplier_id" name="supplier_id">
-                        <option value="">- Semua Supplier -</option>
-                        @foreach($supplier as $s)
-                            <option value="{{ $s->supplier_id }}">{{ $s->supplier_nama }}</option>
-                        @endforeach
-                    </select>
-                    <small class="form-text text-muted">Filter berdasarkan Supplier</small>
-                </div>
-            </div>
-        </div>
-
-        <table class="table table-bordered table-striped table-hover table-sm" id="table_stok">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Barang</th>
-                    <th>User</th>
-                    <th>Supplier</th>
-                    <th>Tanggal Masuk</th>
-                    <th>Jumlah</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-        </table>
-    </div>
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog"
-         data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
-</div>
-@endsection
-
-@push('css')
-{{-- Jika butuh CSS khusus silakan taruh di sini --}}
-@endpush
-
-@push('js')
-<script>
-function modalAction(url = '') {
-    $('#myModal').load(url, function () {
-        $('#myModal').modal('show');
-    });
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<style>
+body{
+    font-family: "Times New Roman", Times, serif;
+    margin: 6px 20px 5px 20px;
+    line-height: 15px;
 }
-var dataStok;
-
-$(document).ready(function() {
-    dataStok = $('#table_stok').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: {
-            url: "{{ url('stok/list') }}", // Pastikan route ini sesuai
-            type: "POST",
-            data: function (d) {
-                d.barang_id   = $('#barang_id').val();
-                d.user_id     = $('#user_id').val();
-                d.supplier_id = $('#supplier_id').val();
-            }
-        },
-        columns: [
-            {
-                data: "DT_RowIndex",
-                className: "text-center",
-                orderable: false,
-                searchable: false
-            },
-            {
-                // Menampilkan nama barang (asumsikan relasi: stok -> barang)
-                data: "barang.barang_nama",
-                orderable: true,
-                searchable: true
-            },
-            {
-                // Menampilkan username user (asumsikan relasi: stok -> user)
-                data: "user.username",
-                orderable: true,
-                searchable: true
-            },
-            {
-                // Menampilkan nama supplier (asumsikan relasi: stok -> supplier)
-                data: "supplier.supplier_nama",
-                orderable: true,
-                searchable: true
-            },
-            {
-                data: "stok_tanggal",
-                orderable: true,
-                searchable: true
-            },
-            {
-                data: "stok_jumlah",
-                orderable: true,
-                searchable: true
-            },
-            {
-                data: "aksi",
-                className: "",
-                orderable: false,
-                searchable: false
-            }
-        ]
-    });
-
-    // Reload DataTables jika filter berubah
-    $('#barang_id, #user_id, #supplier_id').on('change', function() {
-        dataStok.ajax.reload();
-    });
-
-    $('#table_stok_filter input').unbind().bind().on('keyup', function(e){
-        if(e.keyCode == 13){ // enter key
-            dataStok.search(this.value).draw();
-        }
-    });
-});
-</script>
-@endpush
+table {
+    width:100%;
+    border-collapse: collapse;
+}
+td, th {
+    padding: 4px 3px;
+}
+th{
+    text-align: left;
+}
+.d-block{
+    display: block;
+}
+img.image{
+    width: auto;
+    height: 80px;
+    max-width: 150px;
+    max-height: 150px;
+}
+.text-right {
+    text-align: right;
+}
+.text-center {
+    text-align: center;
+}
+.p-1{
+    padding: 5px 1px 5px 1px;
+}
+.font-10{
+    font-size: 10pt;
+}
+.font-11{
+    font-size: 11pt;
+}
+.font-12{
+    font-size: 12pt;
+}
+.font-13{
+    font-size: 13pt;
+}
+.border-bottom-header{
+    border-bottom: 1px solid;
+}
+.border-all, .border-all th, .border-all td{
+    border: 1px solid;
+}
+</style>
+</head>
+<body>
+<table class="border-bottom-header">
+    <tr>
+        <td width="15%" class="text-center">
+            <img src="{{ asset('polinema-bw.jpeg') }}" class="image">
+        </td>
+        <td width="85%">
+            <span class="text-center d-block font-11 font-bold mb-1">KEMENTERIAN PENDIDIKAN, KEBUDAYAAN, RISET, DAN TEKNOLOGI</span>
+            <span class="text-center d-block font-13 font-bold mb-1">POLITEKNIK NEGERI MALANG</span>
+            <span class="text-center d-block font-10">Jl. Soekarno-Hatta No. 9 Malang 65141</span>
+            <span class="text-center d-block font-10">Telepon (0341) 404424 Pes. 101-105, 0341-404420, Fax. (0341) 404420</span>
+            <span class="text-center d-block font-10">Laman: www.polinema.ac.id</span>
+        </td>
+    </tr>
+</table>
+<h3 class="text-center">LAPORAN DATA STOCK BARANG</h3>
+<table class="border-all">
+    <thead>
+        <tr>
+            <th class="text-center">No</th>
+            <th>Nama Barang</th>
+            <th>Nama User</th>
+            <th>Nama Supplier</th>
+            <th class="text-right">Tanggal Masuk</th>
+            <th class="text-right">Jumlah Stock</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($stok as $s)
+        <tr>
+            <td class="text-center">{{ $loop->iteration }}</td>
+            <td>{{ $s->barang->barang_nama }}</td>
+            <td>{{ $s->user->nama }}</td>
+            <td>{{ $s->supplier->supplier_nama }}</td>
+            <td class="text-right">{{ $s->stok_tanggal }}</td>
+            <td class="text-right">{{ $s->stok_jumlah }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+</body>
+</html>
